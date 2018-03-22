37c94e699e2ce78b4ac4769140e94b6db4b510db
Raul Victoria Martin <raul.victoria@avanzit.com>
2018-03-22 16:39:19 +0100

    Bug fixing

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,11 +6,6 @@ done
 
 mkdir -p $GONG_REPORTE_LOG
 
-until nc -z $GONG_REPORTE_DB_HOST $MYSQL_PORT; do
-    echo "$(date) - waiting for mysql..."
-    sleep 1
-done
-
 	read -d '' gor_db <<EOF
 create DATABASE /*!32312 IF NOT EXISTS*/ $GONG_REPORTE_DB_NAME;
 grant ALL ON $GONG_REPORTE_DB_NAME.* TO '$GONG_REPORTE_DB_USER'@'%' IDENTIFIED BY '$GONG_REPORTE_DB_PASSWORD';
@@ -20,12 +15,11 @@ EOF
 	echo "$gor_db" > ./gor_db.sql
 
 
-mysql -h $GONG_REPORTE_DB_HOST -u root -p$MYSQL_ROOT_PASSWORD < ./gor_db.sql
+mysql -h $GONG_REPORTE_DB_HOST -u $GONG_REPORTE_DB_USER -p$GONG_REPORTE_DB_PASSWORD < ./gor_db.sql
 
 mysql -h $GONG_REPORTE_DB_HOST -u $GONG_REPORTE_DB_USER -p$GONG_REPORTE_DB_PASSWORD < /tmp/gongr/script_bbdd/gongr_v1.0.1.sql
 
-#rm ./gor_db.sql
-
+rm ./gor_db.sql
 
 app_config_path=$TMP_DIR/src/main/resources
 
